5dcac26f2c4cd6bc269ac5a0ab7896406ed8184d
chana-rozin <c0556787069@gmail.com>
2024-10-27 15:51:04 +0200

    finish

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import style from './Home.module.css'
 
 function Home() {
   return (
-    <div>
+    <div className={style.container}>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
         Dolore dicta veritatis necessitatibus id quibusdam ad incidunt enim suscipit, error,<br />
         minima ducimus sequi molestiae cum eum. Odit ea quo magni consequatur.</p>
